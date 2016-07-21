4ddbd96458fa49ac56dbf5a7f62248a9af2e3646
r-raymond <robin@robinraymond.de>
2016-07-21 18:27:57 +0200

    adds preamble of config file

--- mail-server.nix
+++ mail-server.nix
@@ -1,1 +1,29 @@
-
+let
+  domain = "example.com";
+  host_prefix = "mail";
+  login_accounts = [ "user1" "user2" ];
+  vmail_id_start = 5000;
+  vmail_user_name = "vmail";
+  vmail_group_name = "vmail";
+  mail_dir = "/var/vmail";
+  cert_file = "mail-server.crt";
+  key_file = "mail-server.key";
+in
+let
+  vmail_user = [{
+    name = vmail_user_name;
+    isNormalUser = false;
+    uid = vmail_id_start;
+    home = mail_dir;
+    createHome = true;
+    group = vmail_group_name;
+  }];
+  accountsToUser = x: {
+    name = x + "@" + domain;
+    isNormalUser = false;
+    group = vmail_group_name;
+  };
+  mail_user = map accountsToUser login_accounts;
+in
+{
+}
