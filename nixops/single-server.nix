4427a308e30bdb764f07eb0839ce55f06d2764d0
Robin Raymond <robin@robinraymond.de>
2017-09-13 10:16:47 +0200

    flesh out nixops file a little

--- nixops/single-server.nix
+++ nixops/single-server.nix
@@ -11,6 +11,18 @@
         mailserver = {
           enable = true;
           domain = "example.com";
+
+          hostPrefix = "mail";
+          loginAccounts = {
+              user1 = {
+                  hashedPassword = "$6$/z4n8AQl6K$kiOkBTWlZfBd7PvF5GsJ8PmPgdZsFGN1jPGZufxxr60PoR0oUsrvzm2oQiflyz5ir9fFJ.d/zKm/NgLXNUsNX/";
+              };
+          };
+          virtualAliases = {
+              info = "user1";
+              postmaster = "user1";
+              abuse = "user1";
+          };
         };
     };
 }
