7c1e153d859b5b0e4c4ab8a78aa241cf667bb03d
Robin Raymond <robin@robinraymond.de>
2017-09-14 19:58:57 +0200

    add sending and receiving by imap test

--- tests/extern.nix
+++ tests/extern.nix
@@ -32,6 +32,9 @@ import ./../../nixpkgs/nixos/tests/make-test.nix {
                   user1 = {
                       hashedPassword = "$6$/z4n8AQl6K$kiOkBTWlZfBd7PvF5GsJ8PmPgdZsFGN1jPGZufxxr60PoR0oUsrvzm2oQiflyz5ir9fFJ.d/zKm/NgLXNUsNX/";
                   };
+                  user2 = {
+                      hashedPassword = "$6$u61JrAtuI0a$nGEEfTP5.eefxoScUGVG/Tl0alqla2aGax4oTd85v3j3xSmhv/02gNfSemv/aaMinlv9j/ZABosVKBrRvN5Qv0";
+                  };
               };
 
               enableImap = true;
@@ -39,7 +42,7 @@ import ./../../nixpkgs/nixos/tests/make-test.nix {
         };
       client = { config, pkgs, ... }:
       {
-        environment.systemPackages =with pkgs; [ fetchmail ];
+        environment.systemPackages = with pkgs; [ fetchmail msmtp procmail ];
       };
     };
 
@@ -47,8 +50,37 @@ import ./../../nixpkgs/nixos/tests/make-test.nix {
   let
     fetchmailRc =
     ''
-    poll SERVER with proto IMAP
-        user 'user1\@example.com' there with password 'user1' is 'root' here
+        poll SERVER with proto IMAP
+            user 'user1\@example.com' there with password 'user1' is 'root' here
+            mda procmail
+    '';
+
+    procmailRc =
+    ''
+        DEFAULT=\$HOME/mail
+    '';
+
+    msmtpRc = 
+    ''
+        account        test
+        host           SERVER
+        port           587
+        from           user2\@example.com
+        user           user2\@example.com
+        password       user2
+    '';
+    email1 =
+    ''
+        From: User2 <user2\@example.com>
+        To: User1 <user1\@example.com>
+        Cc:
+        Bcc:
+        Subject: This is a test Email from user2 to user1
+        Reply-To:
+
+        Hello User1,
+
+        how are you doing today?
     '';
   in
     ''
@@ -57,12 +89,28 @@ import ./../../nixpkgs/nixos/tests/make-test.nix {
       $server->waitForUnit("multi-user.target");
       $client->waitForUnit("multi-user.target");
 
-      subtest "imap", sub {
+      subtest "imap retrieving mail", sub {
           $client->succeed("echo '${fetchmailRc}' > ~/.fetchmailrc");
+          $client->succeed("echo '${procmailRc}' > ~/.procmailrc");
           $client->succeed("sed -i s/SERVER/`getent hosts server | awk '{ print \$1 }'`/g ~/.fetchmailrc");
           $client->succeed("chmod 0700 ~/.fetchmailrc");
           $client->succeed("cat ~/.fetchmailrc >&2");
+          # fetchmail returns EXIT_CODE 1 when no new mail
           $client->succeed("fetchmail -v || [ \$? -eq 1 ] >&2");
+      };
+
+      subtest "submission port send mail", sub {
+          $client->succeed("echo '${msmtpRc}' > ~/.msmtprc");
+          $client->succeed("sed -i s/SERVER/`getent hosts server | awk '{ print \$1 }'`/g ~/.msmtprc");
+          $client->succeed("cat ~/.msmtprc >&2");
+          $client->succeed("echo '${email1}' > mail.txt");
+          # send email from user2 to user1
+          $client->succeed("msmtp -a test --tls=on --tls-certcheck=off --auth=on user1\@example.com < mail.txt >&2");
+      };
+
+      subtest "imap retrieving mail 2", sub {
+          # fetchmail returns EXIT_CODE 0 when it retrieves mail
+          $client->succeed("fetchmail -v >&2");
       };
     '';
 }
