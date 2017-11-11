76fc3d67ae855de80fc9523add5bc5a41ec8bc5d
John Boehr <jbboehr@gmail.com>
2017-11-11 09:53:35 +0000

    Fix syntax error in sample nixops config

--- nixops/single-server.nix
+++ nixops/single-server.nix
@@ -11,7 +11,7 @@
         mailserver = {
           enable = true;
           fqdn = "mail.example.com";
-          domains = [ "example.com", "example2.com" ];
+          domains = [ "example.com" "example2.com" ];
           loginAccounts = {
               "user1@example.com" = {
                   hashedPassword = "$6$/z4n8AQl6K$kiOkBTWlZfBd7PvF5GsJ8PmPgdZsFGN1jPGZufxxr60PoR0oUsrvzm2oQiflyz5ir9fFJ.d/zKm/NgLXNUsNX/";
