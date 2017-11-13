f9289240495a5fad96febc5876a21d19fdac2f2c
Ruben Maher <r@rkm.id.au>
2017-11-14 08:16:59 +1030

    Add debug option for verbose logging

--- default.nix
+++ default.nix
@@ -256,6 +256,16 @@ in
 
       '';
     };
+
+    debug = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable verbose logging for mailserver related services.  This
+        intended be used for development purposes only, you probably don't want
+        to enable this unless you're hacking on nixos-mailserver.
+      '';
+    };
   };
 
   imports = [
