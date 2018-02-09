b75575f02e90c208bdd68549e3967ad7fb3bfd1d
Robin Raymond <robin@robinraymond.de>
2018-02-09 15:02:28 +0100

    remove unbound

--- mail-server/rmilter.nix
+++ mail-server/rmilter.nix
@@ -50,9 +50,6 @@ in
         enable = true;
     };
 
-    services.unbound.enable = true;
-    networking.nameservers = [ "127.0.0.1" ];
-
     services.rmilter = {
       inherit debug;
       enable = true;
